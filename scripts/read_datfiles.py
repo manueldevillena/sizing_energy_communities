f4261e9bbeee8fa48cc25377f9cf140a34409b0a
manueldevillena <m.manueldevillena@gmail.com>
2021-11-25 17:58:12 +0000

    Delete scripts directory

--- scripts/read_datfiles.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-if __name__ == "__main__":
-
-    paths = ['hauts_sarts/datfiles/datscenario{}.dat'.format(i) for i in range(25)]
-    production_profiles = pd.DataFrame()
-    for file in paths:
-        p_profile = pd.read_csv(file, header=None, sep=' ', squeeze=True)[2]
